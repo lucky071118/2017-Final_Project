9fb950907d6f1bb9a129c4e2579bad2ff41dc30e
yuchi <lucky071118@gmail.com>
2017-12-28 19:52:25 +0800

    update Enemy about initial and move

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,4 +1,26 @@
 
-public class Enemy {
+public class Enemy extends GameObject{
+	
+	private final int INITIAL_X = 400;
 
+    public Enemy(int x, int y){
+        super(x, y);
+        initAlien();
+    }
+    
+    private void  initAlien(){
+        
+        loadImage("com\\moving_sprite\\alien.png");
+        getImageDimensions();
+        
+    }
+    
+    public void move() {
+
+        if (x < 0) {
+            x = INITIAL_X;
+        }
+
+        x -= 1;
+    }
 }
