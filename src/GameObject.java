64a3cf925c3f6a6184e4652b3bd62c55c21f6414
yuchi <lucky071118@gmail.com>
2017-12-28 18:18:13 +0800

    create gameobject

---
+++ src/GameObject.java
@@ -0,0 +1,60 @@
+import java.awt.Image;
+import java.awt.Rectangle;
+
+import javax.swing.ImageIcon;
+
+public class GameObject {
+	
+	//object coordinates  
+	protected int x;
+    protected int y;
+    
+    //object width and height
+    protected int width;
+    protected int height;
+    
+    //whether object is visible or not
+    protected boolean isVisible;
+    
+    //image of object 
+    protected Image image;
+    
+    public GameObject(int x, int y) {
+    	this.x = x;
+    	this.y = y;
+    }
+    
+    protected void loadImage(String imageName){
+        ImageIcon imageIcon = new ImageIcon(imageName);
+        image = imageIcon.getImage();
+    }
+    
+    protected void getImageDimensions(){
+        width = image.getWidth(null);
+        height = image.getHeight(null);
+    }
+    
+    public Image getImage() {
+        return image;
+    }
+    
+    public int getX(){
+        return x;
+    }
+    
+    public int getY(){
+        return y;
+    }
+    
+    public boolean isVisible() {
+        return isVisible;
+    }
+    
+    public void setVisible(Boolean visible) {
+    	isVisible = visible;
+    }
+    
+    public Rectangle getBounds(){
+        return new Rectangle(x, y, width, height);
+    }
+}
