a94ee4991c08853a92d3ada1a25c423f0dfe13eb
yuchi <lucky071118@gmail.com>
2017-12-28 23:45:24 +0800

    create enemyA

---
+++ src/EnemyA.java
@@ -0,0 +1,14 @@
+
+public class EnemyA extends Enemy{
+	
+	public EnemyA(int x, int y) {
+		super( x, y, 60, 1);
+		
+	}
+	
+	
+	protected String getImageName() {
+		return "bin\\EnemyA.png";
+	}
+	
+}
